d023294b7cc022ac4f64a3e00d0f43f6531ba763
cameroncates <kamrankpk1998@gmail.com>
2020-09-13 22:32:27 +0500

    make it better

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { href:"https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css", rel: "stylesheet"},
       { href:"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", rel: "stylesheet"},
       { href: "https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.min.css", rel: "stylesheet"},
-      { href: "http://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css", rel: "stylesheet"},
+      { href: "https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css", rel: "stylesheet"},
       { src: "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.87/pdf.js"},
       { src: "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.4.456/build/pdf.min.js"},
       { src: "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"}
